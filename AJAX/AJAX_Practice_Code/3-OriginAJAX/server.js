204c373c4fc47d37f0748ff52bdb4bc774236222
sherry-Zheng <674868933@qq.com>
2023-02-10 20:11:24 +0800

    AJAX-Part2

--- AJAX/AJAX_Practice_Code/3-OriginAJAX/server.js
+++ AJAX/AJAX_Practice_Code/3-OriginAJAX/server.js
@@ -1,5 +1,6 @@
 //NMD去学node.js和ES6箭头函数了
 // 1. 引入express
+const { request, response } = require('express');
 const express = require('express');
 
 //2. 创建应用对象
@@ -13,18 +14,48 @@ app.get('/server',(request, response)=>{
     response.setHeader('Access-Control-Allow-Origin','*');
     
     //设置响应体
-    response.send('HELLO AJAX');
+    response.send('HELLO AJAX-2');
 
 });
 
 app.post('/server',(request, response)=>{
     //设置响应头 设置允许跨域
     response.setHeader('Access-Control-Allow-Origin', '*');
-    //遇到自定义响应头时可以设置
-    response.setHeader('Access-Control-Allow-Headers', '*');
+    // //遇到自定义响应头时可以设置
+    // response.setHeader('Access-Control-Allow-Headers', '*');
     
     //设置响应体
     response.send('HELLO AJAX');
+
+});
+
+//可以接收任何类型的请求
+app.all('server',(request,response)=>{
+});
+
+//JSON响应
+app.all('/json-server',(request,response){
+});
+
+//针对 IE 缓存
+app.get('/ie', (request, response) => {
+    //设置响应头 设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+
+    //设置响应体
+    response.send("HELLO IE - 5");
+
+});
+
+//针对 请求超时：作延时响应：2s
+app.get('/delay', (request, response) => {
+    //设置响应头 设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    setTimeout(() => {
+        //设置响应体
+        response.send("延时响应");
+    }, 3000)
+
 
 });
 
