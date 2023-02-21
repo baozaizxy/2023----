7842383b8f68575204fcfa8470e3ccba99e8055e
sherry-Zheng <674868933@qq.com>
2023-02-21 18:51:34 +0800

    2-21

--- vue_test/s/components/Test.vue
+++ vue_test/s/components/Test.vue
@@ -1,6 +1,7 @@
 <template>
 	<div>
 		<button @click="isShow = !isShow">显示/隐藏</button>
+		<!-- 直接写appear可等价为 :appear="true" -->
 		<transition name="hello" appear>
 			<h1 v-show="isShow">你好啊！</h1>
 		</transition>
@@ -23,11 +24,14 @@
 		background-color: orange;
 	}
 
+    /* 没取名字用默认的v-enter-active 取了名字就要用名字 */
 	.hello-enter-active{
 		animation: atguigu 0.5s linear;
+		/* linear 匀速 */
 	}
 
 	.hello-leave-active{
+		/* anomation 动画 */
 		animation: atguigu 0.5s linear reverse;
 	}
 
